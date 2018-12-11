27657024bfe6d717b6c7e0b3c085b9e54d5acd58
Matthew Alexander <alexandermatthew12@gmail.com>
2018-12-11 21:23:18 +0700

    Added code to check number of messages per hour

--- DataflowPipeline/src/Main.java
+++ DataflowPipeline/src/Main.java
@@ -1,4 +1,3 @@
-
 import org.bson.BsonArray;
 import org.bson.BsonValue;
 import org.bson.codecs.*;
@@ -7,14 +6,33 @@ import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.json.JsonReader;
 
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static java.util.Arrays.asList;
 
 public class Main {
 
+    private static String toDateHourString(Date date) {
+        String dateHour = Integer.toString(date.getMonth()) + " Date: " + Integer.toString(date.getDate())
+                + " Hour: " + Integer.toString(date.getHours());
+        return dateHour;
+    }
+
+    private boolean compareDateHour(Date date1, Date date2) {
+        if (date1.getYear() == date2.getYear() && date1.getMonth() == date2.getMonth()) {
+            if (date1.getDate() == date2.getDate() && date1.getHours() == date2.getHours()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
         String path = "newChats.json";
+
         // take all text and put it into a string
         Scanner scanner = new Scanner(new File(path));
         String jsonText = scanner.useDelimiter("\\A").next();
@@ -44,9 +62,8 @@ public class Main {
 //        bw.flush();
 //        bw.close();
 
-
         //System.out.println(jsonFinalText);
-        HashMap<Integer, HashSet<String>> numAgentsInHour = new HashMap<>();
+        Hashtable<String, Integer> numAgentsInHour = new Hashtable<>();
 
         // read through all chats.json
         final CodecRegistry codecRegistry = CodecRegistries.fromProviders(asList(new ValueCodecProvider(),
@@ -58,12 +75,29 @@ public class Main {
 
         BsonArray docArray = arrayReader.decode(reader, DecoderContext.builder().build());
 
-        for (BsonValue doc : docArray.getValues()) {
+        int hourIndex = 0;
+        for (BsonValue document : docArray.getValues()) {
             //System.out.println(doc);
-//            if (doc.asDocument().get("agent"))
-            System.out.println(doc.asDocument().get("createdAt").asDateTime());
+            long milliseconds = document.asDocument().get("createdAt").asDateTime().getValue();
+            Date date = new Date(milliseconds);
+
+            DateFormat simple = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z");
+
+            Integer val = numAgentsInHour.get(toDateHourString(date));
+
+            if (val != null)
+                numAgentsInHour.put(toDateHourString(date), val + 1);
+            else
+                numAgentsInHour.put(toDateHourString(date), 1);
+
         }
-//
+
+        for (Map.Entry<String, Integer> entry : numAgentsInHour.entrySet()) {
+            System.out.println(entry.getKey() + " Value: " + entry.getValue());
+        }
+
+
+
 //        Document doc = Document.parse(text);
 //
 //        System.out.println(doc.toJson());
