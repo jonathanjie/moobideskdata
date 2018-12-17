e765d93b433b9488269ae1f2506c0f635230e555
Matthew Alexander <alexandermatthew12@gmail.com>
2018-12-17 23:41:42 +0700

    Refactored code

--- DataflowPipeline/src/Main.java
+++ DataflowPipeline/src/Main.java
@@ -6,6 +6,7 @@ import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.json.JsonReader;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -14,12 +15,12 @@ import static java.util.Arrays.asList;
 
 public class Main {
 
-    public static final String OUTPUT_FILE_PATH = "E:/Moobidesk/Moobidesk/moobideskdata/DataflowPipeline/output.txt";
+    private static final String OUTPUT_FILE_PATH = "E:/Moobidesk/Moobidesk/moobideskdata/DataflowPipeline/outputUpdatedAt.txt";
+    private static final String INPUT_JSON_FILE_NAME = "newChats.json";
 
     private static String toDateHourString(Date date) {
-        String dateHour = "Month: " + Integer.toString(date.getMonth()) + " Date: " + Integer.toString(date.getDate())
+        return "Month: " + Integer.toString(date.getMonth()) + " Date: " + Integer.toString(date.getDate())
                 + " Hour: " + Integer.toString(date.getHours());
-        return dateHour;
     }
 
     private boolean compareDateHour(Date date1, Date date2) {
@@ -32,45 +33,43 @@ public class Main {
         return false;
     }
 
-    public static void main(String[] args) throws IOException {
-        String path = "newChats.json";
+    private static void convertJsonToArray(String jsonText) throws IOException {
+        String arrStrings[] = jsonText.split("}");
 
+        for (int i = 0; i < arrStrings.length - 2; i++) {
+            arrStrings[i] +=  "},";
+        }
+
+        arrStrings[arrStrings.length - 2] += "}]";
+
+        StringBuilder jsonFinalText = new StringBuilder();
+        for (String str : arrStrings) {
+            jsonFinalText.append(str);
+        }
+
+        jsonFinalText.insert(0, "[");
+
+        // to write big files
+        FileOutputStream fos = new FileOutputStream(INPUT_JSON_FILE_NAME);
+        OutputStreamWriter w = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+        BufferedWriter bw = new BufferedWriter(w);
+
+        bw.write(jsonFinalText.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
         // take all text and put it into a string
-        Scanner scanner = new Scanner(new File(path));
+        Scanner scanner = new Scanner(new File(INPUT_JSON_FILE_NAME));
         String jsonText = scanner.useDelimiter("\\A").next();
         scanner.close();
 
-//        String arrStrings[] = jsonText.split("}");
-//
-//        for (int i = 0; i < arrStrings.length - 2; i++) {
-//            arrStrings[i] +=  "},";
-//        }
-//
-//        arrStrings[arrStrings.length - 2] += "}]";
-//
-//        String jsonFinalText = "";
-//        for (String str : arrStrings) {
-//            jsonFinalText += str;
-//        }
-//
-//        jsonFinalText = "[" + jsonFinalText;
-//
-//        // to write big files
-//        FileOutputStream fos = new FileOutputStream("newChats.json");
-//        OutputStreamWriter w = new OutputStreamWriter(fos, "UTF-8");
-//        BufferedWriter bw = new BufferedWriter(w);
-//
-//        bw.write(jsonFinalText);
-//        bw.flush();
-//        bw.close();
-
-        //System.out.println(jsonFinalText);
         TreeMap<String, Integer> messagesEveryHour = new TreeMap<>();
 
         // read through all chats.json
         final CodecRegistry codecRegistry = CodecRegistries.fromProviders(asList(new ValueCodecProvider(),
-                new BsonValueCodecProvider(),
-                new DocumentCodecProvider()));
+                new BsonValueCodecProvider(), new DocumentCodecProvider()));
 
         JsonReader reader = new JsonReader(jsonText);
         BsonArrayCodec arrayReader = new BsonArrayCodec(codecRegistry);
@@ -79,11 +78,12 @@ public class Main {
 
         for (BsonValue document : docArray.getValues()) {
             //System.out.println(doc);
-            long milliseconds = document.asDocument().get("createdAt").asDateTime().getValue();
+            long milliseconds = document.asDocument().get("updatedAt").asDateTime().getValue();
             Date date = new Date(milliseconds);
 
-            DateFormat simple = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z");
+//            DateFormat simple = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z");
 
+            // calculate messagesEveryHour
             Integer val = messagesEveryHour.get(toDateHourString(date));
 
             if (val != null)
@@ -91,15 +91,19 @@ public class Main {
             else
                 messagesEveryHour.put(toDateHourString(date), 1);
 
+
+            // calculate 
         }
 
+        int totalChats = 0;
         StringBuilder lineContent = new StringBuilder();
         for (Map.Entry<String, Integer> entry : messagesEveryHour.entrySet()) {
             lineContent.append(entry.getKey()).append(" Number of messages: ").append(entry.getValue()).append("\n");
+            totalChats += entry.getValue();
         }
 
         BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH));
-        System.out.println("Printing");
+        System.out.println("Total chats: " + Integer.toString(totalChats));
         writer.write(lineContent.toString());
         writer.close();
 
